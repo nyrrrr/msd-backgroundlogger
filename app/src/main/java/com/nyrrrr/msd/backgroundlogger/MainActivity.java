1c55a05b8b20fed679e070e484d668dfcb13754d
nyrrrr <info@nyrrrr.com>
2016-11-24 22:25:19 +0100

    minor changes

--- app/src/main/java/com/nyrrrr/msd/backgroundlogger/MainActivity.java
+++ app/src/main/java/com/nyrrrr/msd/backgroundlogger/MainActivity.java
@@ -1,13 +1,18 @@
 package com.nyrrrr.msd.backgroundlogger;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
+    private Intent oIntent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        oIntent = new Intent(this, SensorDataLogger.class);
+        startService(oIntent);
     }
 }
--- app/src/main/java/com/nyrrrr/msd/backgroundlogger/SensorDataLogger.java
+++ app/src/main/java/com/nyrrrr/msd/backgroundlogger/SensorDataLogger.java
@@ -4,25 +4,39 @@ import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.widget.Toast;
 
 /**
  * Created by nyrrrr on 24.11.2016.
  */
 
 public class SensorDataLogger extends Service {
-    @Nullable
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onCreate() {
+        super.onCreate();
     }
 
     @Override
     public int onStartCommand(Intent intent, int flags, int startId) {
-        return super.onStartCommand(intent, flags, startId);
+        Toast.makeText(this, "SERVICE STARTED", Toast.LENGTH_SHORT).show();
+        return Service.START_STICKY; // restart when killed
     }
 
     @Override
     public void onLowMemory() {
-        super.onLowMemory();
+        Toast.makeText(getApplicationContext(), "Memory low", Toast.LENGTH_SHORT).show();
+        super.onLowMemory(); // TODO do not kill
+    }
+
+    @Override
+    public void onDestroy() {
+        Toast.makeText(this, "DESTROYED", Toast.LENGTH_SHORT).show();
+        super.onDestroy(); // TODO backup data?
+    }
+
+    @Nullable
+    @Override
+    public IBinder onBind(Intent intent) {
+        return null;
     }
 }
